5cbb688910eada898b6ea73c00faaa80e6cfb05e
suhanisahu490-boop <suhanisahu490@gmail.com>
2026-07-30 14:32:17 +0530

    Done

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DashboardLayout from '../Components/DashboardLayout';
-import '../Pages/CSS/products.css';
+import '../Pages/CSS/Product.css';
 import { useNavigate } from 'react-router-dom';
 
 const Products = () => {
