fcd60b10547f5925d658e59608aaa8a91c69188a
suhanisahu490-boop <suhanisahu490@gmail.com>
2026-09-01 10:10:26 +0530

    Add build for GitHub Pages

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import DashboardLayout from '../Components/DashboardLayout'
 
 const Contacts = () => {
+  const [message, setMessage] = useState([])
+  const api_url = import.meta.env.VITE_API_URL 
+
+  const fetchMessage = async () => {
+    try {
+      const res = await axios.get(`${api_url}/api/message/get-message`)
+      setMessage(res.data.contacts)
+      console.log(res);
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchMessage()
+  }, [])
+
   return (
     <>
-    <DashboardLayout>
-        
-    </DashboardLayout>
+      <DashboardLayout>
+        <table border='1' cellSpacing='0' cellPadding='10'>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone </th>
+            <th>Message</th>
+          </tr>
+          {
+            message?.map((item) => (
+              <tr key={item._id}>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>{item.message}</td>
+              </tr>
+            ))
+          }
+        </table>
+      </DashboardLayout>
     </>
   )
 }
